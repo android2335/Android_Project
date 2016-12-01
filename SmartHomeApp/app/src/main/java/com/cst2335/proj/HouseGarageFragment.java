fc67c9cd6b4d126893905cc60f021bbcc55ab259
Yu Hou <yuhou531@gmail.com>
2016-11-30 19:21:27 -0500

    add back to main buttons in fragments

--- SmartHomeApp/app/src/main/java/com/cst2335/proj/HouseGarageFragment.java
+++ SmartHomeApp/app/src/main/java/com/cst2335/proj/HouseGarageFragment.java
@@ -1,12 +1,25 @@
 package com.cst2335.proj;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HouseGarageFragment extends Fragment {
+
+    private static final String TAG = HouseGarageFragment.class.getSimpleName();
+    private Button mainButton;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.i(TAG, "onAttach()");
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,7 +29,19 @@ public class HouseGarageFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView()");
+
         View theView = inflater.inflate(R.layout.fragment_house_garage, container, false);
+        mainButton = (Button) theView.findViewById(R.id.houseGarageMainButton);
+
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), NavigateToolbar.class);
+                startActivity(intent);
+            }
+        });
+
         return theView;
     }
 }
